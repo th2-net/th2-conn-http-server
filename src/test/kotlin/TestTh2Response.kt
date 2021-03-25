49db4ddf778d37d8dbc18ac7bf418347146ab049
Topru <topru123@gmail.com>
2021-03-25 15:24:48 +0300

    [TH2-1528] Th2Response tests update

--- src/test/kotlin/TestTh2Response.kt
+++ src/test/kotlin/TestTh2Response.kt
@@ -11,13 +11,11 @@
  * limitations under the License.
  */
 
-import com.exactpro.th2.common.grpc.Direction
-import com.exactpro.th2.common.grpc.RawMessage
+import com.exactpro.th2.common.grpc.*
 import com.exactpro.th2.common.message.addField
 import com.exactpro.th2.common.message.message
 import com.exactpro.th2.httpserver.server.responses.Th2Response
 import com.google.protobuf.ByteString
-import mu.KotlinLogging
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
@@ -25,24 +23,154 @@ class TestTh2Response {
 
     @Test
     fun th2ResponseTest() {
-        val responseMessage = message("Response", Direction.FIRST, "somealias").apply {
-            addField("code", 500)
-            addField("reason", "Test reason")
-            metadataBuilder.protocol = "http"
-            metadataBuilder.putProperties("uuid", "test-uuid")
-        }.build()
+        // Auto generation of content length and content type overwrite check
+        var response = Th2Response.Builder().setHead(
+            createHeadMessage(
+                500, uuid = "test-uuid",
+                reason = "Test reason"
+            )
+        ).setBody(
+            createBodyMessage(
+                body = "SOME BYTES".toByteArray(),
+                contentType = "application"
+            )
+        ).build()
 
-        val bodyMessage = RawMessage.newBuilder().apply {
-            body = ByteString.copyFrom("SOME BYTES".toByteArray())
-            metadata = metadataBuilder.putProperties("contentType", "application").build()
-        }.build()
-
-        val response = Th2Response.Builder().setHead(responseMessage).setBody(bodyMessage).build()
         Assertions.assertEquals(500, response.statusCode)
         Assertions.assertEquals("Test reason", response.startLine.reason)
         Assertions.assertEquals("test-uuid", response.uuid)
         Assertions.assertEquals("application", response.headers["content-type"][0])
         Assertions.assertEquals("10", response.headers["content-length"][0])
 
+        // Overwrite check
+        response = Th2Response.Builder().setHead(
+            createHeadMessage(
+                404, uuid = "0-0-0-0-0",
+                reason = "Non",
+                contentLength = 5
+            )
+        ).setBody(
+            createBodyMessage(
+                body = "SOME BYTES".toByteArray()
+            )
+        ).build()
+        Assertions.assertEquals(404, response.statusCode)
+        Assertions.assertEquals("Non", response.startLine.reason)
+        Assertions.assertEquals("0-0-0-0-0", response.uuid)
+        Assertions.assertEquals(emptyList<String>(), response.headers["content-type"])
+        Assertions.assertEquals("5", response.headers["content-length"][0])
+
+
+        // Auto generation check
+        response = Th2Response.Builder().setHead(
+            createHeadMessage(
+                uuid = "0-0-0-0-0",
+                contentLength = 4
+            )
+        ).setBody(
+            createBodyMessage(
+                body = "SOME BYTES".toByteArray(),
+            )
+        ).build()
+
+        Assertions.assertEquals(200, response.statusCode)
+        Assertions.assertEquals("OK", response.startLine.reason)
+        Assertions.assertEquals("0-0-0-0-0", response.uuid)
+        Assertions.assertEquals(emptyList<String>(), response.headers["content-type"])
+        Assertions.assertEquals("4", response.headers["content-length"][0])
     }
+
+    @Test
+    fun th2ErrorTest() {
+        val stateException = IllegalStateException().javaClass
+        val argumentException = IllegalArgumentException().javaClass
+
+        // Test for UUID required
+        Assertions.assertThrows(
+            stateException,
+            { Th2Response.Builder().setHead(createHeadMessage()).build() },
+            "UUID must be non null, Th2Response must throw error"
+        )
+
+        // Test for Response header message type required
+        Assertions.assertThrows(
+            stateException,
+            { Th2Response.Builder().setHead(createHeadMessage(uuid = "0-0-0-0-0", headType = "WrongType")) },
+            "Type of head message must be response, Th2Response must throw error if there another type of message"
+        )
+
+        // Test in group for Response header message type required
+        Assertions.assertThrows(
+            stateException,
+            {
+                Th2Response.Builder().setGroup(
+                    MessageGroup.newBuilder()
+                        .addMessages(
+                            AnyMessage.newBuilder()
+                                .mergeMessage(createHeadMessage(uuid = "0-0-0-0-0", headType = "WrongType")).build()
+                        )
+                        .addMessages(AnyMessage.newBuilder().mergeRawMessage(createBodyMessage()).build())
+                        .build()
+                )
+            },
+            "Type of head message must be response, Th2Response must throw error if there another type of message"
+        )
+
+        // Test for message type check in the group, raw or parsed
+        Assertions.assertThrows(
+            argumentException,
+            {
+                Th2Response.Builder().setGroup(
+                    MessageGroup.newBuilder()
+                        .addMessages(AnyMessage.newBuilder().mergeRawMessage(createBodyMessage()).build())
+                        .addMessages(
+                            AnyMessage.newBuilder()
+                                .mergeMessage(createHeadMessage(uuid = "0-0-0-0-0", headType = "WrongType")).build()
+                        )
+                        .build()
+                )
+            },
+            "Type of head message must be response, Th2Response must throw error if there another type of message"
+        )
+
+
+    }
+
+    private fun createHeadMessage(
+        code: Int? = null,
+        reason: String? = null,
+        uuid: String? = null,
+        contentLength: Int? = null,
+        headType: String = "Response",
+    ): Message {
+        return message(headType, Direction.FIRST, "test").apply {
+            code?.let { this.addField("code", code) }
+            reason?.let { this.addField("reason", reason) }
+            this.metadataBuilder.protocol = "http"
+            if (contentLength != null) {
+                this.addField(
+                    "headers", listOf(
+                        message("header").apply {
+                            this.addField("name", "Content-Length")
+                            this.addField("value", contentLength)
+                        }.build()
+                    )
+                )
+            }
+            uuid?.let { this.metadataBuilder.putProperties("uuid", it) }
+
+        }.build()
+    }
+
+    private fun createBodyMessage(
+        body: ByteArray = "".toByteArray(),
+        contentType: String? = null,
+    ): RawMessage {
+        return RawMessage.newBuilder().apply {
+            this.body = ByteString.copyFrom(body)
+            if (contentType != null) this.metadata = metadataBuilder.putProperties("contentType", contentType).build()
+
+        }.build()
+    }
+
 }
